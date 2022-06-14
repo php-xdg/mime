c7d962c9411dce378bdeb4d7b6b1961e17ca806a
ju1ius <jules.bernable@gmail.com>
2022-06-14 04:55:49 +0200

    use MimeDatabaseGenerator for default db generation

--- scripts/build-default-db.php
+++ scripts/build-default-db.php
@@ -1,19 +1,16 @@
 <?php declare(strict_types=1);
 
-use ju1ius\XDGMime\Compiler\MimeDatabaseCompiler;
-use ju1ius\XDGMime\Parser\MimeDatabaseParser;
+use ju1ius\XDGMime\MimeDatabaseGenerator;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $xmlFile = __DIR__ . '/../resources/shared-mime-info/data/freedesktop.org.xml.in';
 $outputDir = __DIR__ . '/../src/Resources/db';
 
-if (!is_dir($outputDir)) {
-    mkdir($outputDir, 0o755, true);
-}
-$parser = new MimeDatabaseParser();
-$compiler = new MimeDatabaseCompiler();
-$compiler->compileToDirectory(
-    $parser->parse($xmlFile),
-    $outputDir,
-);
+$generator = (new MimeDatabaseGenerator())
+    ->useXdgDirectories(false)
+    ->addCustomPaths($xmlFile)
+;
+$generator->generate($outputDir);
+
+exit(0);
